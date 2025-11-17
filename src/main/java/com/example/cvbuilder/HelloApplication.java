9a09cccf3939d7caea02ae0330d69ef2f74ee8ba
Shahiraalima <shahiraurbi@gmail.com>
2025-11-17 18:56:54 +0600

    Fixed the size of the stage

--- src/main/java/com/example/cvbuilder/HelloApplication.java
+++ src/main/java/com/example/cvbuilder/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.cvbuilder;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setResizable(false);
+        stage.setWidth(600);
+        stage.setHeight(600);
+        stage.setTitle("CVBuilder");
         stage.setScene(scene);
         stage.show();
     }
