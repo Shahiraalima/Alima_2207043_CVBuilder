03db8bb2438bfaff33d9275d60fd4ca78fecf8a6
Shahiraalima <shahiraurbi@gmail.com>
2025-11-17 21:35:20 +0600

    Updated Scene3

--- src/main/java/com/example/cvbuilder/Scene3Controller.java
+++ src/main/java/com/example/cvbuilder/Scene3Controller.java
@@ -1,7 +1,11 @@
 package com.example.cvbuilder;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class Scene3Controller {
     @FXML
+    private AnchorPane anchorPane;
+    @FXML
+
 }
